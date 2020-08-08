c8bed879866ec01396a933842a01c12dd4c770d2
Atul Rauthan <atulrauthan7@gmail.com>
2020-08-08 22:20:10 +0530

    Added hero banner

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <img className="home__image"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/AugART/GW/Hero/ShopNow/fst/rec/DesktopHero_3000x1200__Default-1._CB408331804_.jpg" 
+            alt="" />
+        </div>
+    )
+}
+
+export default Home
